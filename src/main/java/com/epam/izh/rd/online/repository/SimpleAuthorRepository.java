7fc0f1e8fad85636736a00b51881bc75933a311b
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-07-13 22:49:36 +0300

    Add method remove

--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -29,7 +29,27 @@ public class SimpleAuthorRepository implements AuthorRepository {
 
     @Override
     public boolean remove(Author author) {
-        return false;
+        if (findByFullName(author.getName(), author.getLastName()) == null) {
+            return false;
+        }
+        int positionOfDeleteAuthor = -1;
+        for (int i = 0; i < authors.length; i++) {
+            if (author.getName().equals(authors[i].getName()) &&
+                    author.getLastName().equals(authors[i].getLastName())) {
+                positionOfDeleteAuthor = i;
+            }
+        }
+        Author[] authorsCopy = new Author[authors.length - 1];
+
+        for (int i = 0; i < authors.length; i++) {
+            if (i <= positionOfDeleteAuthor) {
+                authorsCopy[i] = authors[i];
+            } else if (i > positionOfDeleteAuthor) {
+                authorsCopy[i] = authors[i - 1];
+            }
+        }
+        authors = authorsCopy;
+        return true;
     }
 
     @Override
