a588cac037cf0b0c0ae1a64f9e2400f5158f415a
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-07-14 10:25:20 +0300

    Добавил реализацию методов

--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -1,25 +1,57 @@
 package com.epam.izh.rd.online.repository;
 
+import com.epam.izh.rd.online.entity.Author;
 import com.epam.izh.rd.online.entity.SchoolBook;
 
 public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
+    private SchoolBook[] schoolBooks = new SchoolBook[0];
+
     @Override
     public boolean save(SchoolBook book) {
-        return false;
+        SchoolBook[] schoolBooksCopy = new SchoolBook[schoolBooks.length + 1];
+        System.arraycopy(schoolBooks, 0, schoolBooksCopy, 0, schoolBooks.length);
+        schoolBooksCopy[schoolBooksCopy.length - 1] = book;
+        return true;
     }
 
     @Override
     public SchoolBook[] findByName(String name) {
-        return new SchoolBook[0];
+        SchoolBook[] schoolBooksCopy = new SchoolBook[schoolBooks.length];
+        int countFoundBooks = 0;
+
+        for (SchoolBook schoolBook : schoolBooks) {
+            if (schoolBook.getName().equals(name)) {
+                schoolBooksCopy[countFoundBooks] = schoolBook;
+                countFoundBooks++;
+            }
+        }
+
+        SchoolBook[] foundSchoolBooks = new SchoolBook[countFoundBooks];
+        System.arraycopy(schoolBooksCopy, 0, foundSchoolBooks, 0, countFoundBooks);
+
+        return foundSchoolBooks;
     }
 
     @Override
     public boolean removeByName(String name) {
-        return false;
+        if (findByName(name).length <= 0) {
+            return false;
+        }
+        int countFoundBooks = 0;
+        for (int i = 0; i < schoolBooks.length; i++) {
+            if (schoolBooks[i].getName().equals(name)) {
+                countFoundBooks++;
+            } else {
+                schoolBooks[i - countFoundBooks] = schoolBooks[i];
+            }
+        }
+        SchoolBook[] schoolBooksCopy = new SchoolBook[schoolBooks.length - countFoundBooks];
+        System.arraycopy(schoolBooks, 0, schoolBooksCopy, 0, schoolBooks.length - countFoundBooks);
+        return true;
     }
 
     @Override
     public int count() {
-        return 0;
+        return schoolBooks.length;
     }
 }
